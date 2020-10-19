f8fdf543f9eb865cb89ba23b2b166a846d64abf1
unalun <unal.un@hotmail.com>
2020-10-19 21:30:00 +0300

    all oroject

---
+++ AProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AProductAPI.Controllers
+{
+    [Route("/api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string> { "product1", "product2" };
+
+        }
+    }
+}
---
+++ BProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BProductAPI.Controllers
+{
+    [Route("/api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string> { "product3", "product4" };
+
+        }
+    }
+}
